c6bb9f04bfe77d90e17b1724d2746ebba8d71ed6
Jhon Melendez <jhonmelendez77@gmail.com>
2024-05-10 15:46:02 -0500

    Leer datos desde una base de datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,22 @@
 package org.example;
 
+import model.Employee;
+import repository.EmployeeRepository;
+import repository.Repository;
+import util.DataBaseConnection;
+
 import java.sql.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/project";
-        String user = "root" ;
-        String pass = "" ;
-        try (Connection myConn =DriverManager.getConnection(url,user,pass);
-             Statement myStamt =myConn.createStatement();
-             ResultSet myRes =myStamt.executeQuery("SELECT * FROM employees");
-             ){
 
-            while (myRes.next()){
-                System.out.println(myRes.getString("first_name"));
-            }
+        try (Connection myConn = DataBaseConnection.getInstance();){
+            Repository<Employee> repository = new EmployeeRepository();
 
-        }catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Algo salio mal :(");
+            repository.findAll().forEach(System.out::println);
+
         }
 
 
--- src/main/java/util/DataBaseConnection.java
+++ src/main/java/util/DataBaseConnection.java
@@ -1,4 +1,19 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    private static String url  = "jdbc:mysql://localhost:3306/project";
+    private static String user = "root";
+    private static String pass = "";
+    private static Connection myConn;
+
+    public static Connection getInstance() throws SQLException {
+        if(myConn == null){
+            myConn = DriverManager.getConnection(url,user,pass);
+        }
+        return myConn;
+    }
 }
